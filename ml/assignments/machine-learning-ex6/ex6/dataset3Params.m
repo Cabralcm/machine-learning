9c6a57df5424f34db6e7b72e81409ebdc1952f06
engbrianlee <engbrianlee@gmail.com>
2017-08-04 16:12:00 -0400

    Add files via upload
    Support Vector Machines

--- ml/assignments/machine-learning-ex6/ex6/dataset3Params.m
+++ ml/assignments/machine-learning-ex6/ex6/dataset3Params.m
@@ -8,8 +8,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -22,8 +21,23 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+C = [0.01;0.03;0.1;0.3;1;3;10;30];
+sigma = [0.01;0.03;0.1;0.3;1;3;10;30];
+[C,sigma] = meshgrid(C, sigma);
+C_sigma_pairs = [C(:), sigma(:)];  
 
-
+prediction = zeros(size(C_sigma_pairs,1), size(yval, 1));
+prediction_error = zeros(size(prediction,1), 1);
+for i = 1:size(prediction,1)
+  C = C_sigma_pairs(i, 1); sigma = C_sigma_pairs(i, 2);
+  model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+  prediction(i, :) = svmPredict(model, Xval)';
+  prediction_error(i) = mean(double(prediction(i,:)' ~= yval));
+  printf(['Prediction_error: %f\tC: %f\tSigma: %f\t'], prediction_error(i), C, sigma);
+end
+ [~, lowest_error_index] =  min(prediction_error);
+ C = C_sigma_pairs(lowest_error_index, 1);
+ sigma = C_sigma_pairs(lowest_error_index, 2);
 
 
 
